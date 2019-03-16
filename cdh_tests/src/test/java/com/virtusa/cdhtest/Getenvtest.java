acb07d2ad9e7a7cad499f6936c934bd0836d6b11
prabhu <pshankar@virtusa.com>
2019-03-15 21:21:22 -0400

    more tests

---
+++ cdh_tests/src/test/java/com/virtusa/cdhtest/Getenvtest.java
@@ -0,0 +1,23 @@
+package com.virtusa.cdhtest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Getenvtest {
+  @Test
+  public void f() {
+	  System.out.print("Successfully run one test");
+	  
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  
+  
+}
---
+++ cdh_tests/src/test/java/com/virtusa/cdhtest/HiveParamstest.java
@@ -0,0 +1,39 @@
+package com.virtusa.cdhtest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import com.virtusa.cdhtest.*;
+
+public class HiveParamstest {
+	 public ClouderaHiveJdbcTest cdhtest;
+	 
+	@BeforeTest
+	  public void beforeTest() {
+
+	
+		
+		  cdhtest =  new ClouderaHiveJdbcTest( "select * from order_items limit 50",
+				  "192.168.1.227",
+				  "10000",
+				  "org.apache.hive.jdbc.HiveDriver",
+				  "cloudera",
+				  "cloudera");
+		  //cdhtest.runQuery();
+		 
+	  }
+	
+  @Test
+  public void dbcheck() throws Exception {
+	
+	  cdhtest.runQuery();
+	  	
+  }
+ 
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
